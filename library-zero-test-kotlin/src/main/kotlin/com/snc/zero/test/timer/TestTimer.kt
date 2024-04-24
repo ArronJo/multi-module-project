4a6eec4aa1aa3ea2593cf3254347362f4c063ae1
mcharima5 <mcharima5@gmail.com>
2024-04-24 10:01:34 +0900

    feat: 아차...test module 을 머지 안했네

---
+++ library-zero-test-kotlin/src/main/kotlin/com/snc/zero/test/timer/TestTimer.kt
@@ -0,0 +1,17 @@
+package com.snc.zero.test.timer
+
+import com.snc.zero.test.extensions.debug.toSecDisplay
+import kotlin.math.max
+
+class TestTimer {
+
+    private var startTime: Long = 0
+
+    fun start() {
+        startTime = System.currentTimeMillis()
+    }
+
+    fun stop(): String {
+        return max(System.currentTimeMillis() - startTime, 0).toSecDisplay()
+    }
+}
