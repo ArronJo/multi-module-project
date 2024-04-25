3398df343ce0e1233d9efad0996cdd2ab43211fe
mcharima5 <mcharima5@gmail.com>
2024-04-25 16:59:52 +0900

    feat: 로그 출력을 위한 신규 함수 추가 - (Array<T>, ByteArray).toStrings()

---
+++ library-zero-core-kotlin/src/main/kotlin/com/snc/zero/core/extensions/text/toStringExt.kt
@@ -0,0 +1,27 @@
+package com.snc.zero.core.extensions.text
+
+fun <T> Array<T>.toStrings(): String {
+    val sb = StringBuilder()
+    for (v in this) {
+        if (sb.isNotEmpty()) {
+            sb.append(", ")
+        }
+        sb.append(v)
+    }
+    sb.insert(0, "[")
+    sb.append("]")
+    return sb.toString()
+}
+
+fun ByteArray.toStrings(): String {
+    val sb = StringBuilder()
+    for (v in this) {
+        if (sb.isNotEmpty()) {
+            sb.append(", ")
+        }
+        sb.append(v.toHexString())
+    }
+    sb.insert(0, "[")
+    sb.append("]")
+    return sb.toString()
+}
