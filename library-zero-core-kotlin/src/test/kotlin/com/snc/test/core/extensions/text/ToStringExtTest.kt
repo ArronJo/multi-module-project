8dc9446bcf272e1a196b402d94d2b2c86feecbae
mcharima5 <mcharima5@gmail.com>
2024-04-26 09:08:31 +0900

    feat: 로그 출력을 위한 신규 함수 추가 - (Array<T>, ByteArray).toStrings()

---
+++ library-zero-core-kotlin/src/test/kotlin/com/snc/test/core/extensions/text/ToStringExtTest.kt
@@ -0,0 +1,38 @@
+package com.snc.test.core.extensions.text
+
+import com.snc.zero.core.extensions.text.toStrings
+import com.snc.zero.logger.jvm.TLogging
+import com.snc.zero.test.base.BaseTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+private val logger = TLogging.logger { }
+
+class ToStringExtTest : BaseTest() {
+
+    @Test
+    fun `toStrings - Array`() {
+        // given
+        val data = mutableListOf<String>()
+        data.add("A")
+        data.add("a")
+        data.add("1")
+        data.add("가")
+        // when
+        val v1 = data.toTypedArray().toStrings()
+        // then
+        logger.debug { "toStrings - Array 결과: $v1" }
+        assertEquals(v1, "[A, a, 1, 가]")
+    }
+
+    @Test
+    fun `toStrings - ByteArray`() {
+        // given
+        val data = "Aa1가"
+        // when
+        val v1 = data.toByteArray().toStrings()
+        // then
+        logger.debug { "toStrings - ByteArray 결과: $v1" }
+        assertEquals(v1, "[41, 61, 31, ea, b0, 80]")
+    }
+}
