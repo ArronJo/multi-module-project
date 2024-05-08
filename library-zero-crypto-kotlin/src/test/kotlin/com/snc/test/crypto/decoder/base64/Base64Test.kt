160fd8b86ab5fb02db5b195a3f3d9073acbbbc32
mcharima5 <mcharima5@gmail.com>
2024-05-08 12:15:03 +0900

    feat: Base64 Test 추가

---
+++ library-zero-crypto-kotlin/src/test/kotlin/com/snc/test/crypto/decoder/base64/Base64Test.kt
@@ -0,0 +1,40 @@
+package com.snc.test.crypto.decoder.base64
+
+import com.snc.zero.crypto.encoder.base64.Base64
+import com.snc.zero.logger.jvm.TLogging
+import com.snc.zero.test.base.BaseTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+private val logger = TLogging.logger { }
+
+class Base64Test : BaseTest() {
+
+    @Test
+    fun `Decode To Base64`() {
+        // given
+        val data = "YXNkc25jeW5oIDI5ODR5aGQ4OWB5dTg5ODkxODl1OWpxZmRhc2pnZnVpYXNnZHM="
+        // when
+        val v = String(Base64.decode(data))
+        val v2 = String(java.util.Base64.getDecoder().decode(data))
+        // then
+        logger.debug { "Base64 decoded v1: $v" }
+        logger.debug { "Base64 decoded v2: $v2" }
+        //assertEquals(v, "asdsncynh 2984yhd89`yu8989189u9jqfdasjgfuiasgds")
+        assertEquals(v, v2)
+    }
+
+    @Test
+    fun `Decode To Base64 (Safe URL)`() {
+        // given
+        val data = "U3BlY2lhbCBjaGFyczogw7vDu8O/IGhlcmUu"   // -> "Special chars: ûÿ here."
+        // when
+        val v = String(Base64.decode(data))
+        val v2 = String(java.util.Base64.getDecoder().decode(data))
+        // then
+        logger.debug { "Base64 decoded v1: $v" }
+        logger.debug { "Base64 decoded v2: $v2" }
+        //assertEquals(v, "Special chars: ûûÿ here.")
+        assertEquals(v, v2)
+    }
+}
---
+++ library-zero-crypto-kotlin/src/test/kotlin/com/snc/test/crypto/encoder/base64/Base64Test.kt
@@ -0,0 +1,41 @@
+package com.snc.test.crypto.encoder.base64
+
+import com.snc.zero.crypto.encoder.base64.Base64
+import com.snc.zero.logger.jvm.TLogging
+import com.snc.zero.test.base.BaseTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+
+private val logger = TLogging.logger { }
+
+class Base64Test : BaseTest() {
+
+    @Test
+    fun `Encode To Base64`() {
+        // given
+        val data = "asdsncynh 2984yhd89`yu8989189u9jqfdasjgfuiasgds"
+        // when
+        val v = Base64.encode(data.toByteArray())
+        val v2 = java.util.Base64.getEncoder().encodeToString(data.toByteArray())
+        // then
+        logger.debug { "Base64 encoded 1: $v" }
+        logger.debug { "Base64 encoded 2: $v2" }
+        //assertEquals(v, "YXNkc25jeW5oIDI5ODR5aGQ4OWB5dTg5ODkxODl1OWpxZmRhc2pnZnVpYXNnZHM=")
+        assertEquals(v, v2)
+    }
+
+    @Test
+    fun `Encode To Base64 (Safe URL)`() {
+        // given
+        val data = "Special chars: \u00fb\u00fb\u00ff here."
+        // when
+        val v = Base64.encode(data.toByteArray())
+        val v2 = String(java.util.Base64.getEncoder().encode(data.toByteArray()))
+        // then
+        // Safe URL Base64 인코딩 처리되면서  "/" -> "_" 로 변경되므로 값이 다르게 표현된다.
+        logger.debug { "Base64 encoded 1: $v" }  // "U3BlY2lhbCBjaGFyczogw7vDu8O_IGhlcmUu"
+        logger.debug { "Base64 encoded 2: $v2" } // "U3BlY2lhbCBjaGFyczogw7vDu8O/IGhlcmUu"
+        assertNotEquals(v, v2)
+    }
+}
