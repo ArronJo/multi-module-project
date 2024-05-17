5d95d1a94f6311428c9c4762be716d109e07133d
mcharima5 <mcharima5@gmail.com>
2024-05-17 12:57:55 +0900

    feat: FSDirectory, FSFile 클래스 추가 feat: File 확장 함수 추가

---
+++ library-zero-file-manager-kotlin/src/main/kotlin/com/snc/zero/filemanager/file2/FSFile.kt
@@ -0,0 +1,195 @@
+package com.snc.zero.filemanager.file2
+
+import com.snc.zero.filemanager.file2.extensions.mkdirsOrNot
+import com.snc.zero.logger.jvm.TLogging
+import java.io.*
+import java.nio.CharBuffer
+import java.nio.charset.Charset
+import java.util.*
+
+private val logger = TLogging.logger { }
+
+class FSFile {
+
+    companion object {
+
+        private const val DEFAULT_BUFFER_SIZE = 8 * 1024    // 8kb
+        private const val BUFFER_SIZE_1_MB = 1 * 1024 * 1024    // 1MB = 1024 * 1024 = 1048576
+
+        @JvmStatic
+        @Throws(IOException::class)
+        fun create(file: File, overwrite: Boolean = false): Boolean {
+            val parentFile = file.parentFile
+            if (parentFile != null) {
+                if (!parentFile.mkdirsOrNot()) {
+                    throw IOException("Unable to create parent directory.. $parentFile")
+                }
+            }
+            overwrite(file, overwrite)
+            return file.createNewFile()
+        }
+
+        @JvmStatic
+        private fun overwrite(file: File, overwrite: Boolean = false) {
+            if (file.exists()) {
+                if (!overwrite) {
+                    throw IOException("The source file already exists. $file")
+                } else if (!delete(file)) {
+                    throw IOException("Tried to overwrite the destination, but failed to delete it. $file")
+                }
+            }
+        }
+
+        @JvmStatic
+        @Throws(IOException::class)
+        fun delete(file: File, verbose: Boolean = false): Boolean {
+            if (verbose) {
+                logger.error { "delete : $file" }
+            }
+            return file.delete()
+        }
+
+        @JvmStatic
+        @Throws(IOException::class)
+        fun read(
+            file: File,
+            charset: Charset = charset("UTF-8"),
+            verbose: Boolean = false
+        ): ByteArray {
+            val buf = CharArray(DEFAULT_BUFFER_SIZE)
+            var br: BufferedReader? = null
+            val sb = StringBuilder()
+
+            var length = getLength(file)
+            if (verbose) {
+                logger.info { "read file length $length" }
+            }
+            try {
+                br = BufferedReader(InputStreamReader(FileInputStream(file), charset))
+                while (length > 0) {
+                    val amt = br.read(buf, 0, buf.size)
+                    if (verbose) {
+                        logger.info { "read bytes [$amt / $length]" }
+                    }
+                    if (amt == -1) {
+                        break //throw new IOException("ERROR : " + file + ": unexpected EOF");
+                    }
+                    sb.appendRange(buf, 0, amt)
+                    length -= amt
+                }
+            } catch (e: Exception) {
+                throw IOException("ERROR : $file: trouble reading", e)
+            } finally {
+                //if (verbose) {
+                //    logger.info("read data : \n${String(buffer)}")
+                //}
+                closeQuietly(br)
+            }
+            return sb.toString().toByteArray()
+        }
+
+        @JvmStatic
+        @Throws(IOException::class)
+        fun write(data: ByteArray, out: File, overwrite: Boolean = false): Int {
+            val parentFile = out.getParentFile() ?: return -1
+            if (!parentFile.exists()) {
+                if (!parentFile.mkdirs()) {
+                    return -2
+                }
+            }
+            create(out, overwrite)
+
+            var os: FileOutputStream? = null
+            var offset = 0
+            try {
+                os = FileOutputStream(out)
+                val bufSize = if (data.size > BUFFER_SIZE_1_MB) { // 1 MB 이상일 경우
+                    BUFFER_SIZE_1_MB
+                } else {
+                    DEFAULT_BUFFER_SIZE
+                }
+                val buffer = ByteArray(bufSize)
+                val bis = ByteArrayInputStream(data)
+                var len: Int
+                while (bis.read(buffer, 0, buffer.size).also { len = it } >= 0) {
+                    os.write(buffer, 0, len)
+                    offset += len
+                }
+            } catch (e: IOException) {
+                throw IOException("ERROR : trouble writing $out")
+            } finally {
+                closeQuietly(os)
+            }
+            return offset
+        }
+
+        @JvmStatic
+        @Throws(IOException::class)
+        fun copy(src: File, dst: File, overwrite: Boolean = false): Boolean {
+            try {
+                src.copyTo(target = dst, overwrite = overwrite)
+            } catch (e: IOException) {
+                logger.error(e) { }
+                return false
+            }
+            return true
+        }
+
+        @JvmStatic
+        @Throws(IOException::class)
+        fun copy(input: InputStream, output: OutputStream): Long {
+            var bytesCopied: Long = 0
+            val buffer = ByteArray(8 * 1024)
+            var bytes = input.read(buffer)
+            while (bytes >= 0) {
+                output.write(buffer, 0, bytes)
+                bytesCopied += bytes.toLong()
+                bytes = input.read(buffer)
+            }
+            return bytesCopied
+        }
+
+        @JvmStatic
+        fun toBytes(chars: CharArray): ByteArray {
+            val charBuffer = CharBuffer.wrap(chars)
+            val byteBuffer = Charset.forName("UTF-8").encode(charBuffer)
+            val bytes = Arrays.copyOfRange(
+                byteBuffer.array(),
+                byteBuffer.position(), byteBuffer.limit()
+            )
+            Arrays.fill(byteBuffer.array(), 0.toByte()) // clear sensitive data
+            return bytes
+        }
+
+        @JvmStatic
+        fun getLength(file: File): Int {
+            val longLength = file.length()
+            val length = longLength.toInt()
+            if (length.toLong() != longLength) {
+                throw IOException("ERROR : $file: file too long")
+            }
+            return length
+        }
+
+        private fun closeQuietly(os: OutputStream?) {
+            try {
+                os?.flush()
+            } catch (e: Exception) {
+                logger.error(e) { "Exception" }
+            }
+            try {
+                os?.close()
+            } catch (e: Exception) {
+                logger.error(e) { "Exception" }
+            }
+        }
+
+        private fun closeQuietly(c: Closeable?) {
+            try {
+                c?.close()
+            } catch (e: Exception) {
+                logger.error(e) { "Exception" }
+            }
+        }
+    }
+}
---
+++ library-zero-file-manager-kotlin/src/test/kotlin/com/snc/test/filemanager/file2/FSFileReadTest.kt
@@ -0,0 +1,40 @@
+package com.snc.test.filemanager.file2
+
+import com.snc.zero.filemanager.file2.FSFile
+import com.snc.zero.filemanager.file2.FSInfo
+import com.snc.zero.filemanager.file2.extensions.toFile
+import com.snc.zero.logger.jvm.TLogging
+import com.snc.zero.test.base.BaseJUnit5Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+import java.io.File
+
+private val logger = TLogging.logger { }
+
+class FSFileReadTest : BaseJUnit5Test() {
+
+    private lateinit var parent: File
+    private lateinit var dir: File
+
+    @BeforeEach
+    override fun beforeEach(testInfo: TestInfo) {
+        super.beforeEach(testInfo)
+
+        parent = "/Users/jwjo/Downloads/zzz".toFile()
+        dir = "$parent/ttt".toFile()
+    }
+
+    @Test
+    fun `FSFile read`() {
+        //val file = File(dir, "write_test.js")
+        val file = File(dir, "android.svg")
+        if (!file.exists()) {
+            logger.debug { "$file not exist" }
+            return
+        }
+        val ba = FSFile.read(file, verbose = true)
+        logger.debug { "file size : ${FSInfo.getReadableFileSize(ba.size.toLong())}" }
+        logger.debug { "\n\nfile data : \n${String(ba)}\n-----E.O.D----\n\n" }
+    }
+}
