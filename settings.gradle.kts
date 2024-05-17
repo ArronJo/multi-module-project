4a20cb5ae4b9cb685a434b23d2fe38923565e3e7
mcharima5 <mcharima5@gmail.com>
2024-05-17 13:06:54 +0900

    chore: 프로젝트 정리

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,11 @@ plugins {
 }
 rootProject.name = "multi-module-project"
 
-include(":library-zero-test-kotlin")
-include(":library-zero-logger-kotlin")
 include(":library-zero-core-kotlin")
-include(":library-zero-file-manager-kotlin")
-include(":library-zero-xss-kotlin")
-include(":library-zero-validation-kotlin")
-include(":library-zero-identification-kotlin")
 include(":library-zero-crypto-kotlin")
-include(":library-zero-hangul-kotlin")
-include(":library-zero-age-kotlin")
-
+include(":library-zero-file-manager-kotlin")
+include(":library-zero-identification-kotlin")
+include(":library-zero-logger-kotlin")
+include(":library-zero-test-kotlin")
+include(":library-zero-validation-kotlin")
+include(":library-zero-xss-kotlin")
