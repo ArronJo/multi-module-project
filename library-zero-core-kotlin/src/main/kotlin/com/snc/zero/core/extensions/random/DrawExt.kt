c13797f8365d2a2aeacab8aaf13383b032c1deea
mcharima5 <mcharima5@gmail.com>
2024-05-26 14:28:57 +0900

    fix: 코드 테스트

--- library-zero-core-kotlin/src/main/kotlin/com/snc/zero/core/extensions/random/DrawExt.kt
+++ library-zero-core-kotlin/src/main/kotlin/com/snc/zero/core/extensions/random/DrawExt.kt
@@ -1,13 +1,13 @@
 package com.snc.zero.core.extensions.random
 
-import java.security.SecureRandom
+import java.util.*
 import kotlin.math.ln
 
 fun <T> List<T>.getRandomItem(): T {
     val index = if (this.size > 1) {
         val max = size - 1
         val min = 0
-        SecureRandom().nextInt(max - min + 1) + min
+        SplittableRandom().nextInt(max - min + 1) + min
     } else 0
     return get(index)
 }
@@ -15,7 +15,7 @@ fun <T> List<T>.getRandomItem(): T {
 fun <T> MutableMap<T, Double>.getWeightedRandom(): T? {
     var result: T? = null
     var bestValue = Double.MAX_VALUE
-    val rand = SecureRandom()
+    val rand = SplittableRandom()
     for (element in this.keys) {
         val value = -ln(rand.nextDouble()) / (this[element] ?: 0.0)
         if (value < bestValue) {
