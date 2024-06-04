b715c016803b6765787bd025f8d835d5f5601dd7
mcharima5 <mcharima5@gmail.com>
2024-06-04 17:56:45 +0900

    fix: jacoco #33 #240604

--- library-zero-file-manager-kotlin/src/test/kotlin/com/snc/test/filemanager/file2/FSFileIOTest.kt
+++ library-zero-file-manager-kotlin/src/test/kotlin/com/snc/test/filemanager/file2/FSFileIOTest.kt
@@ -2,7 +2,6 @@ package com.snc.test.filemanager.file2
 
 import com.snc.zero.filemanager.file2.FSFile
 import com.snc.zero.filemanager.file2.extensions.toFile
-import com.snc.zero.logger.jvm.TLogging
 import com.snc.zero.test.base.BaseJUnit5Test
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,10 +9,7 @@ import org.junit.jupiter.api.TestInfo
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.io.IOException
 import java.nio.file.Paths
-
-private val logger = TLogging.logger { }
 
 class FSFileIOTest : BaseJUnit5Test() {
 
@@ -40,27 +36,5 @@ class FSFileIOTest : BaseJUnit5Test() {
     fun `FSFile closeQuietly 2`() {
         val fos: FileOutputStream? = null
         FSFile.closeQuietly(fos)
-    }
-
-    @Test
-    fun `FSFile closeQuietly Exception 1`() {
-        val e = assertThrows(
-            IOException::class.java
-        ) {
-            FSFile.closeQuietly(FileOutputStream(File("/ioa")))
-        }
-        logger.debug { "${e.message}" }
-        assertNotEquals(e.message, "")
-    }
-
-    @Test
-    fun `FSFile closeQuietly Exception 2`() {
-        val e = assertThrows(
-            IOException::class.java
-        ) {
-            FSFile.closeQuietly(FileInputStream(File("/iob")))
-        }
-        logger.debug { "${e.message}" }
-        assertNotEquals(e.message, "")
     }
 }
