3f48356fea8a051d042e2c76e9ea8c693dcd4f30
mcharima5 <mcharima5@gmail.com>
2024-07-15 16:03:46 +0900

    fix: 코드 정리

--- library-zero-core-kotlin/src/main/kotlin/com/snc/zero/core/extensions/random/RandomExt.kt
+++ library-zero-core-kotlin/src/main/kotlin/com/snc/zero/core/extensions/random/RandomExt.kt
@@ -1,6 +1,30 @@
 package com.snc.zero.core.extensions.random
 
 import java.util.*
+import kotlin.jvm.Throws
+import kotlin.math.ln
+
+@Throws(IllegalFormatException::class)
+fun <T> List<T>.getRandomItem(): T {
+    val max = size - 1
+    val min = 0
+    val index = SplittableRandom().nextInt(max - min + 1) + min
+    return get(index)
+}
+
+fun <T> MutableMap<T, Double>.getWeightedRandom(): T? {
+    var result: T? = null
+    var bestValue = Double.MAX_VALUE
+    val rand = SplittableRandom()
+    for (element in this.keys) {
+        val value = -ln(rand.nextDouble()) / this.getOrDefault(element, 0.0)
+        if (value < bestValue) {
+            bestValue = value
+            result = element
+        }
+    }
+    return result
+}
 
 fun randomInt(min: Int = 0, bound: Int): Int {
     val random = SplittableRandom()
--- library-zero-core-kotlin/src/test/kotlin/com/snc/test/core/extensions/random/RandomExtTest.kt
+++ library-zero-core-kotlin/src/test/kotlin/com/snc/test/core/extensions/random/RandomExtTest.kt
@@ -1,6 +1,8 @@
 package com.snc.test.core.extensions.random
 
 import com.snc.zero.core.counter.Counter
+import com.snc.zero.core.extensions.random.getRandomItem
+import com.snc.zero.core.extensions.random.getWeightedRandom
 import com.snc.zero.core.extensions.random.randomInt
 import com.snc.zero.core.extensions.random.randomString
 import com.snc.zero.logger.jvm.TLogging
@@ -120,5 +122,60 @@ class RandomExtTest : BaseJUnit5Test() {
             // then
             logger.debug { "Random String 결과: $v1" }
         }
+    }
+
+    @Test
+    fun `Random Draw Item 1`() {
+        // given
+        val data = listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+        // when
+        for (i in 1..max) {
+            val v1 = data.getRandomItem()
+            counter.put(v1)
+            // then
+            logger.debug { "Random Draw Item 결과: $v1" }
+        }
+        logger.debug { "Random Draw Item 결과: Counter[${counter.size()}, ${counter.total()}] : $counter" }
+    }
+
+    @Test
+    fun `Random Draw Item 2`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            val data = arrayListOf<String>()
+            data.getRandomItem()
+        }
+    }
+
+    @Test
+    fun `Weighted Random Drawing`() {
+        // given
+        val data = hashMapOf<String, Double>()
+        data["김"] = 0.10689
+        data["이"] = 0.07307
+        data["박"] = 0.04192
+        data["정"] = 0.02333
+        data["조"] = 0.01176
+        data["강"] = 0.01055
+        data["장"] = 0.00992
+        data["임"] = 0.00823
+        data["한"] = 0.00773
+        data["류"] = 0.00642
+        data["홍"] = 0.00558
+        data["고"] = 0.00471
+        data["허"] = 0.00326
+        data["남"] = 0.00275
+        data["정"] = 0.00243
+        data["신"] = 0.00192
+        data["엄"] = 0.00144
+        // when
+        for (i in 1..max) {
+            val v1 = data.getWeightedRandom()
+            v1?.let {
+                counter.put(it)
+            }
+            // then
+            logger.debug { "Weighted Random Drawing 결과: $v1" }
+        }
+        logger.debug { "Weighted Random Drawing 결과: Counter[${counter.size()}, ${counter.total()}] : $counter" }
     }
 }
