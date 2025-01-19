d401bda343737b1596ee8272232c8f18b9b6db4a
mcharima5 <mcharima5@gmail.com>
2025-01-19 16:25:51 +0900

    fix: sonar 대응 #18

--- library-zero-hangul-kotlin/src/main/kotlin/com/snc/zero/hangul/matcher/HangulMatcher.kt
+++ library-zero-hangul-kotlin/src/main/kotlin/com/snc/zero/hangul/matcher/HangulMatcher.kt
@@ -2,10 +2,7 @@ package com.snc.zero.hangul.matcher
 
 import com.snc.zero.hangul.compose.HangulDecompose
 import com.snc.zero.hangul.matcher.dto.HangulMatch
-import com.snc.zero.logger.jvm.TLogging
 import java.util.Locale
-
-private val logger = TLogging.logger { }
 
 /**
  * 한글 검색 (초성 검색 포함)
@@ -66,36 +63,5 @@ class HangulMatcher {
             return 1 // match
         }
         return -1 // not match
-    }
-
-    companion object {
-
-        @JvmStatic
-        fun main(args: Array<String>) {
-            println("main $args")
-
-            println("decompose = ${HangulDecompose.decompose('n')}")
-
-            val menus = arrayOf(
-                "사고보험금 신청",
-                "사고보험금 내역조회",
-                "사고보험금 서류보완",
-                "일반보험금 신청",
-                "일반보험금 지급내역",
-                "휴대폰인증 NICE 평가",
-                "휴대폰인증NICE 인증",
-                "휴대폰인증 KCB 평가 (not NICE))",
-                "현대해상은 전용선을 통해 VAN사(NICE)에 카드인증을 요청합니다."
-            )
-
-            val matcher = HangulMatcher()
-            matcher.keyword("ㅈㅇㅅㅇㅌㅎv") // "ㅈ N", "ㅅㄱ")
-            for (m in menus) {
-                val match = matcher.match(m)
-                if (HangulMatch.EMPTY != match) {
-                    logger.info { "검색: $m" }
-                }
-            }
-        }
     }
 }
