3c427d38a7b8653a16e54a6fdc4dfad7e49d329e
mcharima5 <mcharima5@gmail.com>
2025-03-06 15:56:14 +0900

    chore: jacoco test

--- library-zero-pdfbox3-kotlin/src/test/kotlin/com/snc/test/pdfbox/converter/PdfToSvgConverterTest.kt
+++ library-zero-pdfbox3-kotlin/src/test/kotlin/com/snc/test/pdfbox/converter/PdfToSvgConverterTest.kt
@@ -4,25 +4,29 @@ import com.snc.zero.logger.jvm.TLogging
 import com.snc.zero.pdfbox.converter.PdfToSvgConverter
 import com.snc.zero.test.base.BaseJUnit5Test
 import org.junit.jupiter.api.Test
+import java.nio.file.Paths
 
 private val logger = TLogging.logger { }
 
 @Suppress("NonAsciiCharacters")
 class PdfToSvgConverterTest : BaseJUnit5Test() {
 
-    private val inputPdf = "./docs/input/CMS00017.pdf"
+    //private val inputPdf = "./docs/input/CMS00017.pdf"
 
     @Test
     fun `PDF 를 SVG 파일로 추출 - 정상`() {
+        //val inputPdf = File(this::class.java.classLoader.getResource("input/CMS00017.pdf")!!.toURI())
+        val inputPdf = Paths.get(ClassLoader.getSystemResource("input/CMS00017.pdf")!!.toURI())
         val converter = PdfToSvgConverter()
         logger.debug { "inputPdf: $inputPdf" }
-        converter.convert(inputPdf)
+        converter.convert(inputPdf.toString())
     }
 
     @Test
     fun `PDF 를 SVG 파일로 추출 - PDF 타입 아님`() {
+        val inputPdf = Paths.get(ClassLoader.getSystemResource("input/text.txt").toURI())
         val converter = PdfToSvgConverter()
-        converter.convert("./docs/input/text.txt")
+        converter.convert(inputPdf.toString())
     }
 
     @Test
