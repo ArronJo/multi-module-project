f919ce9af4b60f186a008081ad3df7bbee13358f
mcharima5 <mcharima5@gmail.com>
2025-03-26 13:56:54 +0900

    feat: https://www.passay.org/

--- library-zero-password-generator/src/main/kotlin/com/snc/zero/passgen/PwValidator.kt
+++ library-zero-password-generator/src/main/kotlin/com/snc/zero/passgen/PwValidator.kt
@@ -8,37 +8,7 @@ import org.passay.*
  * https://www.passay.org/reference/
  */
 class PwValidator {
-    /*
-    // ConstraintValidator : Spring 클래스
-    class PasswordConstraintsValidator : ConstraintValidator<Password?, String?> {
-        override fun isValid(password: String?, context: ConstraintValidatorContext): Boolean {
-            val passwordValidator = PasswordValidator(
-                listOf( // 비밀번호 길이가 8~20 사이여야 한다.
-                    LengthRule(8, 20), // 적어도 하나의 대문자가 있어야 한다.
-                    CharacterRule(EnglishCharacterData.UpperCase, 1), // 적어도 하나의 소문자가 있어야 한다.
-                    CharacterRule(EnglishCharacterData.LowerCase, 1), // 적어도 하나의 숫자가 있어야 한다.
-                    CharacterRule(EnglishCharacterData.Digit, 1),  // 적어도 하나의 특수문자가 있어야 한다.
-                    CharacterRule(EnglishCharacterData.Special, 1), // 공백문자는 허용하지 않는다.
-                    WhitespaceRule()
-                )
-            )
-            val result = passwordValidator.validate(PasswordData(password))
-            if (result.isValid) {
-                return true
-            }
 
-            context.disableDefaultConstraintViolation()
-            context.buildConstraintViolationWithTemplate(
-                java.lang.String.join(
-                    ",",
-                    passwordValidator.getMessages(result)
-                )
-            )
-                .addConstraintViolation()
-            return false
-        }
-    }
-     */
     companion object {
         fun validate(password: String): ValidateResult {
             val validator = PasswordValidator(
