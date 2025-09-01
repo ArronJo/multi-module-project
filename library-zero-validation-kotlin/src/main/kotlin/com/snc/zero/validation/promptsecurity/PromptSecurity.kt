0b6cad726311c758073dcc75a28fbd68ea5ff152
mcharima5 <mcharima5@gmail.com>
2025-09-01 17:26:41 +0900

    feat: 암호화 로직 추가 및 변경

--- library-zero-validation-kotlin/src/main/kotlin/com/snc/zero/validation/promptsecurity/PromptSecurity.kt
+++ library-zero-validation-kotlin/src/main/kotlin/com/snc/zero/validation/promptsecurity/PromptSecurity.kt
@@ -101,7 +101,7 @@ class PromptSecurity {
             "스크립트_인젝션" to listOf(
                 """(?i)(<script.*?>.*?</script>)""".toRegex(),
                 """(?i)(javascript:|vbscript:|onload=|onclick=)""".toRegex(),
-                """(?i)(<iframe.*?>|<object.*?>|<embed.*?>)""".toRegex()
+                """(?i)(<iframe[^>]*+>|<object[^>]*+>|<embed[^>]*+>)""".toRegex()
             ),
             "특수_문자_인젝션" to listOf(
                 """[\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0B\x0C\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x7F]""".toRegex(),
